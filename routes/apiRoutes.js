55e9fa3f577b08176e7c8bf9ccd6b435a674d68c
Stefanie L <75275831+stefanieklogan@users.noreply.github.com>
2021-03-03 13:14:25 -0500

    delete

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,36 @@
 const path = require('path');
+let dbJSON = require('../db/db.json');
+let fs = require('fs');
 
-////////////get user to api/notes pg
 module.exports = (app) => {
 
-app.get('/api/notes', (req, res) => res.json());
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "../db/db.json"))
+});
+
+app.post('/api/notes', (req, res) => { 
+    const newNote = req.body;
+    req.body.id = dbJSON.length;
+    dbJSON.push(newNote);
+    console.log(dbJSON);
+
+    fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(dbJSON), function (err) {
+        if (err) throw err;
+        res.json("response")
+    })
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+    let idFromBrowser = req.params.id;
+    dbJSON = dbJSON.filter(note=>{
+    
+    return note.id != idFromBrowser;
+    })
+
+    fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(dbJSON), function (err) {
+        if (err) throw err;
+        res.json("response")
+    })
+})
 
 };
